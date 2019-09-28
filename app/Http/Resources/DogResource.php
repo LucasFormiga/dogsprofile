84099e844053f74f61620cb3e252db7565e5212a
Lucas Formiga <lucasformiga@gmail.com>
2019-09-28 15:44:29 -0300

    Refactoring structure to use API Resource

---
+++ app/Http/Resources/DogResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class DogResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+			'id' => $this->id,
+			'name' => $this->name,
+			'breed' => $this->breed,
+			'age' => $this->age,
+			'size' => $this->size,
+			'weight' => $this->weight,
+			'owner' => $this->owner
+		];
+    }
+}
