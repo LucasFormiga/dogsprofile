2ba2cc48124fcb2a1c46555b95c7c6eecff1a7ab
Lucas Formiga <lucasformiga@gmail.com>
2019-10-01 23:34:38 -0300

    Created HomeController for the API

---
+++ app/Http/Controllers/Api/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+class HomeController extends Controller
+{
+	public function index()
+	{
+		return "Hello, world!";
+	}
+}
